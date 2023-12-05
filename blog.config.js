fef15d46a457f746907b1f9bc0973b7dd871a565
2ericbrooks <132639995+2ericbrooks@users.noreply.github.com>
2023-12-05 13:33:45 -0600

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -55,7 +55,7 @@ const BLOG = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    '"Inter+Tight"',
+    '"InterTight"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
