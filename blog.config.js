340e97951ffe23f111a4cdcbf020acb9d07f165e
2ericbrooks <132639995+2ericbrooks@users.noreply.github.com>
2023-12-05 17:25:17 -0600

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,9 +15,9 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。一个普通的干饭人🍚
   GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'hello',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Eric B', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'eriicbrooks', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '""', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://github.com/eriicbrooks/blog', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog', // 网站关键词 英文逗号隔开
