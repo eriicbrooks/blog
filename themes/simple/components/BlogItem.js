1dd99bf313d2f62cf02692951ef60cc135d9ad37
2ericbrooks <132639995+2ericbrooks@users.noreply.github.com>
2023-12-15 17:24:00 -0600

    Update BlogItem.js

--- themes/simple/components/BlogItem.js
+++ themes/simple/components/BlogItem.js
@@ -8,9 +8,9 @@ export const BlogItem = props => {
   const { post } = props
 
   return <div key={post.id} className="h-42 my-6 pb-12 border-b dark:border-gray-800" >
-        {/* 文章标题 */}
+        {/* article title*/}
 
-        <h2 className="mb-2">
+        <h2 className="mb-3">
             <Link
                 href={`${siteConfig('SUB_PATH', '')}/${post.slug}`}
                 className="blog-item-title font-bold text-black text-2xl menu-link">
@@ -22,9 +22,9 @@ export const BlogItem = props => {
 
         <div className="mb-5 text-md text-gray-700 dark:text-gray-300 flex-wrap flex leading-6">
             <div className='space-x-2'>
-                <span>  <a href={siteConfig('SIMPLE_AUTHOR_LINK', null, CONFIG)} className='p-1 hover:text-red-400 transition-all duration-200'><i className="fa-regular fa-user"></i> {siteConfig('AUTHOR')}</a></span>
+                <span>  <a href={siteConfig('SIMPLE_AUTHOR_LINK', null, CONFIG)} className='p-1 hover:text-yellow-400 transition-all duration-200'><i className="fa-regular fa-user"></i> {siteConfig('AUTHOR')}</a></span>
                 <span>
-                    <Link className='p-1 hover:text-red-400 transition-all duration-200' href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}>
+                    <Link className='p-1 hover:text-yellow-400 transition-all duration-200' href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}>
                         <i className="fa-regular fa-clock" /> {post.date?.start_date || post.createdTime}
                     </Link>
                 </span>
@@ -32,8 +32,8 @@ export const BlogItem = props => {
             </div>
 
             <div>
-                {post.category && <Link href={`/category/${post.category}`} className='p-1'> <span className="hover:text-red-400 transition-all duration-200"><i className="fa-regular fa-folder mr-0.5"/>{post.category}</span></Link>}
-                {post?.tags && post?.tags?.length > 0 && post?.tags.map(t => <Link key={t} href={`/tag/${t}`} className=' hover:text-red-400 transition-all duration-200'><span > /{t}</span></Link>)}
+                {post.category && <Link href={`/category/${post.category}`} className='p-1'> <span className="hover:text-yellow-400 transition-all duration-200"><i className="fa-regular fa-folder mr-0.5"/>{post.category}</span></Link>}
+                {post?.tags && post?.tags?.length > 0 && post?.tags.map(t => <Link key={t} href={`/tag/${t}`} className=' hover:text-yellow-400 transition-all duration-200'><span > /{t}</span></Link>)}
             </div>
         </div>
 
@@ -43,7 +43,7 @@ export const BlogItem = props => {
         </div>
 
         <div className='block'>
-            <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} className='inline-block rounded-sm text-blue-600 text-xs dark:border-gray-800 border hover:text-red-400 transition-all duration-200 hover:border-red-300 h-9 leading-8 px-5'>
+            <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} className='inline-block rounded-sm text-yellow-600 text-s dark:border-gray-600 border hover:text-yellow-400 transition-all duration-100 hover:border-yellow-300 h-9 leading-8 px-5'>
                 Continue Reading <i className="fa-solid fa-angle-right align-middle"></i>
             </Link>
         </div>
