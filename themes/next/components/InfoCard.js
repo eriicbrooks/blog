fe7c5035a9808266a5b76f2bda8c9bd0ec59be5a
2ericbrooks <132639995+2ericbrooks@users.noreply.github.com>
2024-01-18 23:32:05 -0600

    Update InfoCard.js

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -7,11 +7,11 @@ const InfoCard = (props) => {
   const { siteInfo } = props
   return <>
     <div className='flex flex-col items-center justify-center '>
-        <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
+        <div className='hover:rotate-0 hover:scale-100 transform duration-100 cursor-pointer' onClick={ () => { Router.push('/') }}>
         <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
         </div>
         <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
-        <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'>{siteConfig('BIO')}</div>
+        <div className='font-light dark:text-white py-2 hover:scale-100 transform duration-200 text-center'>{siteConfig('BIO')}</div>
         <SocialButton/>
     </div>
   </>
