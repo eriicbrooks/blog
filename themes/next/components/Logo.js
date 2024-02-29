f16d45a9a74fe0b064fff5f6cb495b616af4412c
2ericbrooks <132639995+2ericbrooks@users.noreply.github.com>
2024-02-29 14:09:21 -0600

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -5,12 +5,12 @@ const Logo = props => {
   const { className } = props
   return (
         <Link href='/' passHref legacyBehavior>
-            <div className={'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-800 space-y-3 font-bold ' + className}>
+            <div className={'flex flex-col justify-left items-center cursor-pointer bg-black dark:bg-gray-800 space-y-3 ' + className}>
                 <div data-aos="fade-down"
                     data-aos-duration="500"
                     data-aos-once="true"
                     data-aos-anchor-placement="top-bottom"
-                    className='font-serif text-xl text-white'> {siteConfig('TITLE')}</div>
+                    className='font-serif text-white'> {siteConfig('TITLE')}</div>
                 <div data-aos="fade-down"
                     data-aos-duration="500"
                     data-aos-delay="300"
